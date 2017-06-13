a5fbe770fcd7c39c57bba7b7ecf7c6d4a6368cd5
Azis R. Pratama <azis.pratama@gmail.com>
2017-06-14 06:25:26 +0700

    Add files via upload

---
+++ ussd0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ---------------------------------------------
+# Author: Azis R. Pratama
+#
+# Creation date: 05 June 2017
+#
+# Description: Script mmcli untuk mengetahui sisa kuota internet modem.
+#				
+# ---------------------------------------------
+echo ===================================================== 
+echo Mendeteksi perangkat modem ....
+echo ===================================================== 
+sleep 01
+mmcli -L
+sleep 01
+echo ===================================================== 
+echo Memulai perintah MMCLI
+echo =====================================================
+mmcli -m 0 --3gpp-ussd-initiate="*123#"
+sleep 03
+mmcli -m 0 --3gpp-ussd-respond="7"
+sleep 03
+mmcli -m 0 --3gpp-ussd-respond="3"
+sleep 03
+mmcli -m 0 --3gpp-ussd-respond="1"
+sleep 02
+mmcli -m 0 --3gpp-ussd-cancel
+
+
+
+
+
+
